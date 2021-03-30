c31df40979cd943378a42706db3d10c02256a60a
Pasha-From-Russia <none@available.com>
2021-03-30 16:37:38 +0500

    set SIGKILL as default signal

--- SignalsPanel.c
+++ SignalsPanel.c
@@ -20,8 +20,8 @@ in the source distribution for its full text.
 
 Panel* SignalsPanel_new() {
    Panel* this = Panel_new(1, 1, 1, 1, Class(ListItem), true, FunctionBar_newEnterEsc("Send   ", "Cancel "));
-   const int defaultSignal = SIGTERM;
-   int defaultPosition = 15;
+   const int defaultSignal = SIGKILL;
+   int defaultPosition = 9;
    unsigned int i;
    for (i = 0; i < Platform_numberOfSignals; i++) {
       Panel_set(this, i, (Object*) ListItem_new(Platform_signals[i].name, Platform_signals[i].number));
